1cc7ce98ab352d431d12bb9778bb3afd3fc7287a
mahagold11 <*mark.goldhardt@gmail.com*>
2020-02-06 11:47:43 -0700

    axios call and image rendered

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+// import axios from "axios";
+import axios from "axios";
+
+
 import "./App.css";
 
+
 function App() {
+   // Initialize state to hold image
+    
+   const [spacePic, setSpacePic] = useState("");
+
+
+   useEffect (() => {
+     axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => setSpacePic(res.data.url))
+      .catch(err => console.log(err));
+   } ,[]);
+
+
   return (
+   
+
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <img src={spacePic} alt="space pic of the day"/>
+
     </div>
   );
 }
 
 export default App;
+
+
